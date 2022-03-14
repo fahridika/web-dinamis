90d8bfc99a636fd79c486af9a471a77f930819b8
fahridika <96968769+fahridika@users.noreply.github.com>
2022-03-14 13:16:03 +0700

    Add files via upload

---
+++ Toko Baju Muslim/dbconnect.php
@@ -0,0 +1,11 @@
+<?php 
+// isi nama host, username mysql, dan password mysql anda
+$conn = mysqli_connect("localhost","root","","toko_baju");
+
+if(!$conn){
+	echo "Gagal Terkoneksi Database";
+} else {
+	
+};
+
+?>
